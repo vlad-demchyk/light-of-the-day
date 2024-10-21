d8103e6fa1f7db743553045082cba6f68572cc50
vlad-demchyk <167634889+vlad-demchyk@users.noreply.github.com>
2024-10-21 23:41:05 +0200

    wip

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,41 @@
 import "./footer.css";
-import backgroundImage from "../background/background_footer.png"
-import paperForFooter from "../background/paper_for_footer.png"
+import MenuComponent from "./MenuComponent";
+import paperForFooter from "../background/paper_for_footer.png";
+import logo from "../background/logo.png";
 
-
-function Footer(){
-    return( 
-        <footer>
-            {/* <img className="background_footer" src={`${backgroundImage}`}>
-            </img> */}
-            <img className="paper_for_footer" src={`${paperForFooter}`}>
-            </img>
-            <div className="footer_container">
-                    <img className="logo_footer"/>
-                    <div className="contacts_footer"></div>
-                    <nav className="menu-footer"></nav> 
-                    <div className="sub_for_footer"></div>
-            </div>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer>
+      <img className="paper_for_footer" src={`${paperForFooter}`}></img>
+      <div className="footer_container">
+        <img className="logo_footer" src={logo} />
+        <div className="contacts_footer">
+          <p>Phone: +1 (555) 123-4567</p>
+          <p>Email: info@lucedeldiogiorno.com</p>
+          <ul className="social_links">
+            <a href="#">Facebook</a>
+            <a href="#">Inst</a>
+            <a href="#">YouTube</a>
+            <a href="#">Twitter</a>
+          </ul>
+        </div>
+        <div className="menu_footer">
+        <MenuComponent/>
+        </div>
+        
+        <div className="sub_for_footer">
+          <h3>
+            Subscribe to our newsletter and receive A
+            <span style={{ color: "orange" }}> free</span> consultation.
+          </h3>
+          <form className="form_footer">
+            <input type="text" placeholder="Email Address" />
+            <button type="submit">Sing Up</button>
+          </form>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
